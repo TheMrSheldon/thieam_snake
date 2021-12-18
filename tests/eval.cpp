b18a7d2e3c68358d638a339fa13b4a77103a8fb5
Tim Hagen <timhagenmail@googlemail.com>
2021-12-18 21:54:15 +0100

    added additional testcases for scan environment

--- tests/eval.cpp
+++ tests/eval.cpp
@@ -148,3 +148,129 @@ TEST_CASE("Evaluate State1", "[Evaluation]") {
 	CHECK(eval.snakes[0].foodInReach == 1);
 	CHECK(eval.snakes[1].foodInReach == 3);
 }
+
+
+TEST_CASE("Evaluate State2", "[Evaluation]") {
+	/**
+	 * ╔═════════╗	Snake 1:	<11
+	 * ║ . . . . ║	Snake 2:	<22
+	 * ║ . 2 > . ║	Food:		o
+	 * ║ . . 1 > ║	Empty:		.
+	 * ║ . . 1 . ║
+	 * ╚═════════╝
+	 */
+	std::vector<ls::Position> snake1 = {{3,1}, {2,1}, {2,0}};
+	std::vector<ls::Position> snake2 = {{2,2}, {1,2}};
+	std::vector<ls::Position> food = {};
+	auto s1 = ls::Snake(std::move(snake1), ls::Move::up, 100);
+	auto s2 = ls::Snake(std::move(snake2), ls::Move::right, 100);
+	auto state = ls::State(4,4, {s1, s2}, std::move(food));
+
+	//Assert correct setup
+	REQUIRE(state.getWidth() == 4);
+	REQUIRE(state.getHeight() == 4);
+	REQUIRE(state.getSnakes().size() == 2);
+	REQUIRE(state.getSnake(0).getHeadPos() == ls::Position(3,1));
+	REQUIRE(state.getSnake(1).getHeadPos() == ls::Position(2,2));
+
+	//Assert correct evaluation
+	Evaluator evaluator(ls::gm::Arena, state.getSnakes().size(), state.getWidth(), state.getHeight());
+	auto eval = evaluator.evaluate(state);
+	CHECK(eval.winner == ls::SnakeFlags::None);
+	REQUIRE(eval.snakes.size() == 2);
+	CHECK(eval.snakes[0].health == s1.getHealth());
+	CHECK(eval.snakes[1].health == s2.getHealth());
+	/** Board
+	 * ╔═════════╗	Snake 1:	<11
+	 * ║ . . . . ║	Snake 2:	<22
+	 * ║ . 2 > . ║	Food:		o
+	 * ║ . . 1 > ║	Empty:		.
+	 * ║ . . 1 . ║
+	 * ╚═════════╝
+	 * 
+	 * Iterations:
+	 * 0:         	1:         	2:         	3:         	4:         	▓:	Frontier Snake0
+	 * ╔═════════╗	╔═════════╗	╔═════════╗	╔═════════╗	╔═════════╗	▒:	Frontier Snake1
+	 * ║ . . . . ║	║ . . ▒ . ║	║ . ▒ ░ x ║	║ ▒ ░ ░ X ║	║ ░ ░ ░ X ║	█:	Control Snake0
+	 * ║ . 1 2 . ║	║ . ▒ ░ x ║	║ ▒ ░ ░ X ║	║ ░ ░ ░ X ║	║ ░ ░ ░ X ║	░:	Control Snake1
+	 * ║ . . 2 3 ║	║ . . 2 █ ║	║ . ▒ 2 █ ║	║ ▒ ░ x █ ║	║ ░ ░ X █ ║	X:	Fontier Border
+	 * ║ . . 1 . ║	║ . . 1 ▓ ║	║ . . ▓ █ ║	║ . x █ █ ║	║ x X █ █ ║	x:	Control Border
+	 * ╚═════════╝	╚═════════╝	╚═════════╝	╚═════════╝	╚═════════╝
+	 * 
+	 * Result:
+	 * 	Snake | Area control | Border control
+	 * 	=====================================
+	 * 	0    |             3 |             5
+	 * 	1    |             8 |             5
+	 */
+	CHECK(eval.snakes[0].mobility == 3);
+	CHECK(eval.snakes[1].mobility == 8);
+	CHECK(eval.snakes[0].foodInReach == 0);
+	CHECK(eval.snakes[1].foodInReach == 0);
+}
+
+TEST_CASE("Evaluate State3", "[Evaluation]") {
+	/**
+	 * ╔═════════╗	Snake 1:	<11
+	 * ║ . . . . ║	Snake 2:	<22
+	 * ║ . 2 > . ║	Food:		o
+	 * ║ . . 1 > ║	Empty:		.
+	 * ║ 1 1 1 . ║
+	 * ╚═════════╝
+	 */
+	std::vector<ls::Position> snake1 = {{3,1}, {2,1}, {2,0}, {1,0}, {0,0}};
+	std::vector<ls::Position> snake2 = {{2,2}, {1,2}};
+	std::vector<ls::Position> food = {};
+	auto s1 = ls::Snake(std::move(snake1), ls::Move::up, 100);
+	auto s2 = ls::Snake(std::move(snake2), ls::Move::right, 100);
+	auto state = ls::State(4,4, {s1, s2}, std::move(food));
+
+	//Assert correct setup
+	REQUIRE(state.getWidth() == 4);
+	REQUIRE(state.getHeight() == 4);
+	REQUIRE(state.getSnakes().size() == 2);
+	REQUIRE(state.getSnake(0).getHeadPos() == ls::Position(3,1));
+	REQUIRE(state.getSnake(1).getHeadPos() == ls::Position(2,2));
+
+	//Assert correct evaluation
+	Evaluator evaluator(ls::gm::Arena, state.getSnakes().size(), state.getWidth(), state.getHeight());
+	auto eval = evaluator.evaluate(state);
+	CHECK(eval.winner == ls::SnakeFlags::None);
+	REQUIRE(eval.snakes.size() == 2);
+	CHECK(eval.snakes[0].health == s1.getHealth());
+	CHECK(eval.snakes[1].health == s2.getHealth());
+	/** Board
+	 * ╔═════════╗	Snake 1:	<11
+	 * ║ . . . . ║	Snake 2:	<22
+	 * ║ . 2 > . ║	Food:		o
+	 * ║ . . 1 > ║	Empty:		.
+	 * ║ 1 1 1 . ║
+	 * ╚═════════╝
+	 * 
+	 * Iterations:
+	 * 0:         	1:         	2:         	3:         	4:         
+	 * ╔═════════╗	╔═════════╗	╔═════════╗	╔═════════╗	╔═════════╗
+	 * ║ . . . . ║	║ . . ▒ . ║	║ . ▒ ░ x ║	║ ▒ ░ ░ X ║	║ ░ ░ ░ X ║
+	 * ║ . 1 2 . ║	║ . ▒ ░ x ║	║ ▒ ░ ░ X ║	║ ░ ░ ░ X ║	║ ░ ░ ░ X ║
+	 * ║ . . 4 5 ║	║ . . 4 █ ║	║ . ▒ 4 █ ║	║ ▒ ░ 4 █ ║	║ ░ ░ 4 █ ║
+	 * ║ 1 2 3 . ║	║ 1 2 3 ▓ ║	║ 1 2 3 █ ║	║ 1 ▒ 3 █ ║	║ ▒ ░ ▒ █ ║
+	 * ╚═════════╝	╚═════════╝	╚═════════╝	╚═════════╝	╚═════════╝
+	 * 5:         	6:         	▓:	Frontier Snake0
+	 * ╔═════════╗	╔═════════╗	▒:	Frontier Snake1
+	 * ║ ░ ░ ░ X ║	║ ░ ░ ░ X ║	█:	Control Snake0
+	 * ║ ░ ░ ░ X ║	║ ░ ░ ░ X ║	░:	Control Snake1
+	 * ║ ░ ░ ▒ █ ║	║ ░ ░ ░ █ ║	X:	Fontier Border
+	 * ║ ░ ░ ░ █ ║	║ ░ ░ ░ █ ║	x:	Control Border
+	 * ╚═════════╝	╚═════════╝
+	 * 
+	 * Result:
+	 * 	Snake | Area control | Border control
+	 * 	=====================================
+	 * 	0    |             2 |             2
+	 * 	1    |            12 |             2
+	 */
+	CHECK(eval.snakes[0].mobility == 2);
+	CHECK(eval.snakes[1].mobility == 12);
+	CHECK(eval.snakes[0].foodInReach == 0);
+	CHECK(eval.snakes[1].foodInReach == 0);
+}
